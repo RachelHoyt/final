4408359c2d9bfd909eacc36ba69b5f21c658c619
Rachel Hoyt <rahoyt@csustudent.net>
2023-04-07 01:27:12 +0000

    Nav bar

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -6,38 +6,33 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     @base_title = "Soles for Souls"
   end
 
-  test "should get root" do
-    get static_pages_home_url
-    assert_response :success
-  end
-
   test "should get home" do
-    get static_pages_home_url
-    assert_response :success
+    get root_path
+assert_response :success
     assert_select "title", "Home | #{@base_title}"
   end
 
   test "should get events" do
-    get static_pages_events_url
-    assert_response :success
+    get events_path
+assert_response :success
     assert_select "title", "Events | #{@base_title}"
   end
 
   test "should get contact" do
-    get static_pages_contact_url
-    assert_response :success
+    get contact_path
+assert_response :success
     assert_select "title", "Contact | #{@base_title}"
   end
 
   test "should get registration" do
-    get static_pages_registration_url
-    assert_response :success
+    get registration_path
+assert_response :success
     assert_select "title", "Registration | #{@base_title}"
   end
 
   test "should get log_in" do
-    get static_pages_log_in_url
-    assert_response :success
+    get login_path
+assert_response :success
     assert_select "title", "Log In | #{@base_title}"
   end
 end
