1b85e70e96e8642c12ff4ef4cadc3c4d83f2772a
Rachel Hoyt <rahoyt@csustudent.net>
2023-04-13 18:52:05 +0000

    Make a basic User model (including secure passwords)

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   get 'users/new'
   root "static_pages#home"
   get "/events", to: "static_pages#events"
   get "/contact", to: "static_pages#contact"
   get "/registration", to: "static_pages#registration"
-  get "/login", to: "static_pages#log_in"
+  get "/login", to: "sessions#new"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
   get "/signup", to: "users#new"
   get "/more_events", to: "static_pages#more_events"
+  resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
