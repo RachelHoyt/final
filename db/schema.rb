ed77d3e55f70463b8e31ff088ab644fc9ee9adba
Alyssa Simmons <adsimmons2@csustudent.net>
2023-04-20 03:00:45 +0000

    addin image

--- db/schema.rb
+++ db/schema.rb
@@ -10,31 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema[7.0].define(version: 2023_04_19_002513) do
-=======
 ActiveRecord::Schema[7.0].define(version: 2023_04_19_224705) do
->>>>>>> b3fde06ac55816f7acf8465f6797f5ad21ec24c6
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
-<<<<<<< HEAD
     t.boolean "admin"
     t.string "activation_digest"
     t.boolean "activated", default: false
     t.datetime "activated_at"
-=======
-    t.string "activation_digest"
-    t.boolean "activated", default: false
-    t.datetime "activated_at"
-    t.boolean "admin"
     t.string "age"
     t.string "phone"
     t.string "team"
->>>>>>> b3fde06ac55816f7acf8465f6797f5ad21ec24c6
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
